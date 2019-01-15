b8584d93db8fe0c80fdcefbd34a85ccd9c400a22
cbrownll <cbrown7745@gmail.com>
2019-01-15 11:42:43 -0500

    first commit

---
+++ HelloWorld.Tests/ProgramTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace HelloWorld.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Hello_World_Test()
+        {
+            Assert.Equal("Hello", "Hello");
+        }
+        
+
+        
+    }
+}
